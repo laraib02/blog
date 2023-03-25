5add9706f9e7a23f82269d08424dfdfa9d19d0f6
laraib02 <hexacoderx31@gmai.com>
2023-03-25 23:04:19 +0500

    add frontend

---
+++ resources/views/frontend/layouts/nav.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+    <a href="" class="navbar-brand p-0">
+        <h1 class="m-0"><i class="fa fa-server me-3"></i>GreenHost</h1>
+    <!-- <img src="{{asset('frontend/img/logo.png')}}" alt="Logo"> -->
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+            <a href="index.html" class="nav-item nav-link active">Home</a>
+            <a href="about.html" class="nav-item nav-link">About</a>
+            <a href="domain.html" class="nav-item nav-link">Domain</a>
+            <a href="hosting.html" class="nav-item nav-link">Hosting</a>
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                <div class="dropdown-menu m-0">
+                    <a href="team.html" class="dropdown-item">Our Team</a>
+                    <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                    <a href="comparison.html" class="dropdown-item">Comparison</a>
+                </div>
+            </div>
+            <a href="contact.html" class="nav-item nav-link">Contact</a>
+        </div>
+        <butaton type="button" class="btn text-secondary ms-3" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa fa-search"></i></butaton>
+        <a href="" class="btn btn-secondary py-2 px-4 ms-3">Register</a>
+    </div>
+</nav>
+
---
+++ resources/views/frontend/layouts/script.blade.php
@@ -0,0 +1,11 @@
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('frontend/assets/lib/wow/wow.min.js')}}"></script>
+<script src="{{asset('frontend/assets/lib/easing/easing.min.js')}}"></script>
+<script src="{{asset('frontend/assets/lib/waypoints/waypoints.min.js')}}"></script>
+<script src="{{asset('frontend/assets/lib/counterup/counterup.min.js')}}"></script>
+<script src="{{asset('frontend/assets/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+
+<!-- Template Javascript -->
+<script src="{{asset('frontend/assets/js/main.js')}}"></script>
